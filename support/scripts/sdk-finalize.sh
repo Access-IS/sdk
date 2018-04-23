7614b91e09f61c2b82f92fddef9d628bbd92ea07
Lalith Panditharatne <Lalith@access-is.com>
2018-04-23 08:11:45 +0100

    moved sdk location to /opt/sdk
    Signed-off-by: Lalith Panditharatne <Lalith@access-is.com>

--- support/scripts/sdk-finalize.sh
+++ support/scripts/sdk-finalize.sh
@@ -2,17 +2,17 @@
 
 
 
-cp -a output/host output/sdk
+#cp -a output/host output/sdk
 
-cat > output/sdk/env.sh <<EOF
+cat > /opt/sdk/env.sh <<EOF
 export SYSROOT=\$HOME/sdk/arm-buildroot-linux-gnueabihf/sysroot
 export PATH=\$HOME/sdk/bin:$PATH
 export HOST=arm-buildroot-linux-
 EOF
 
-cat .config | gzip -9 > output/sdk/.BRconfig.gz
+cat .config | gzip -9 > /opt/sdk/.BRconfig.gz
 sdk_version=`git rev-parse --short HEAD`
-tar -czf output/images/sdk-$sdk_version.tar.gz -C output/ sdk
+tar -czf output/images/sdk-$sdk_version.tar.gz -C /opt/ sdk
 echo "created sdk : output/images/sdk-$sdk_version.tar.gz"
-rm -rf output/sdk
+#rm -rf output/sdk
 exit 0
